a7208dd6b031157f5db34b6c14d32e9b18c879df
nitink66 <60770391+nitink66@users.noreply.github.com>
2020-03-22 20:55:01 +0530

    Updates

---
+++ precache-manifest.f3f6be33a4f40362143d5c07efc0b265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a51007de5db415499d81560d2bdb19",
+    "url": "/corona-updates/index.html"
+  },
+  {
+    "revision": "e3fc0f0f3825a6d50337",
+    "url": "/corona-updates/static/css/main.c0bbb96c.chunk.css"
+  },
+  {
+    "revision": "efa8b97c355cd7bfc573",
+    "url": "/corona-updates/static/js/2.9e73f1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-updates/static/js/2.9e73f1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fc0f0f3825a6d50337",
+    "url": "/corona-updates/static/js/main.c491cb6d.chunk.js"
+  },
+  {
+    "revision": "8f66f38570d48c6d90f0",
+    "url": "/corona-updates/static/js/runtime-main.8a124cc4.js"
+  },
+  {
+    "revision": "90184294a89137f777d7aec7287eac78",
+    "url": "/corona-updates/static/media/1.90184294.jpg"
+  }
+]);
